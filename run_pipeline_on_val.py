ff5cb1437cc90c84dc8c17628f27b812db6b0704
Adam Wojciechowski <adi61044@gmail.com>
2023-08-05 20:24:02 +0200

    add argparse functionality

--- run_pipeline_on_val.py
+++ run_pipeline_on_val.py
@@ -4,23 +4,12 @@ import pandas as pd
 from tqdm import tqdm
 from numpy import save
 from shutil import copy2
+from argparse import ArgumentParser
 
 
-VAL_SET_PARENT_PATH = "./val"
-VAL_IMAGES_NAMES_FULL_PATH = "./metadata/val_paths_experiments_0.csv" # filtered by class
-MILAN_DESCRIPTIONS_FULL_PATH = "./milan_results/resnet18_imagenet.csv"
-OPENAI_API_TOKEN_FULL_PATH = "/home/adamwsl/.gpt_api_token/token.txt"
-CNN = "resnet18"
-DATASET = "imagenet"
-OUTPUT_PARENT_PATH = "./basic_explanationsxxx"
-EXPLANATION_TYPE = "soft"
-COPY_IMAGE = True
-DATASET_CLASS_NAMES = "./metadata/imagenet_classes.txt"
-
-
-def Main():
+def Main(args):
     # Prepare for processing 
-    if CNN == "resnet18":
+    if args.cnn == "resnet18":
         from torchvision.models import resnet18
         last_layer_name = "layer4"
         model = resnet18(pretrained=True)
@@ -31,7 +20,7 @@ def Main():
                     'layer2' : model.layer2[1].bn2, 
                     'layer3' : model.layer3[1].bn2, 
                     'layer4' : model.layer4[1].bn2}
-    elif CNN == "alexnet":
+    elif args.cnn == "alexnet":
         from torchvision.models import alexnet
         last_layer_name = "conv5"
         model = alexnet(pretrained=True)
@@ -43,36 +32,48 @@ def Main():
                      'conv4' : model.features[8], 
                      'conv5' : model.features[10]}
     
-    df = pd.read_csv(VAL_IMAGES_NAMES_FULL_PATH, header=None)
+    df = pd.read_csv(args.val_images_names_full_path, header=None)
     image_names = df.iloc[:,0].tolist()
     
     # Explain
     for image_name in tqdm(image_names):
-        full_image_path = os.path.join(VAL_SET_PARENT_PATH, image_name)
+        full_image_path = os.path.join(args.val_set_parent_path, image_name)
         
         probabilities, prompt, explanation = run_pipeline_single_decision(model=model, 
                                                                           full_image_path=full_image_path, 
                                                                           layer_name=last_layer_name, 
                                                                           layer_map=layer_map, 
-                                                                          dataset_class_names=DATASET_CLASS_NAMES,
-                                                                          neuron_descriptions_full_path=MILAN_DESCRIPTIONS_FULL_PATH, 
-                                                                          api_token_full_path=OPENAI_API_TOKEN_FULL_PATH, 
-                                                                          explanation_type=EXPLANATION_TYPE)
-        if not os.path.exists(os.path.join(OUTPUT_PARENT_PATH, DATASET, CNN, image_name[:-5])): # [:-5] is for getting rid of ".JPEG"
-            os.makedirs(os.path.join(OUTPUT_PARENT_PATH, DATASET, CNN, image_name[:-5]))
-        save(os.path.join(OUTPUT_PARENT_PATH, DATASET, CNN, image_name[:-5], "probs.npy"), probabilities)
-        with open(os.path.join(OUTPUT_PARENT_PATH, DATASET, CNN, image_name[:-5], "prompt.txt"), "w") as f:
+                                                                          dataset_class_names=args.dataset_class_names,
+                                                                          neuron_descriptions_full_path=args.milan_descriptions_full_path, 
+                                                                          api_token_full_path=args.openai_api_token_full_path, 
+                                                                          explanation_type=args.explanation_type)
+        if not os.path.exists(os.path.join(args.output_parent_path, args.dataset, args.cnn, image_name[:-5])): # [:-5] is for getting rid of ".JPEG"
+            os.makedirs(os.path.join(args.output_parent_path, args.dataset, args.cnn, image_name[:-5]))
+        save(os.path.join(args.output_parent_path, args.dataset, args.cnn, image_name[:-5], "probs.npy"), probabilities)
+        with open(os.path.join(args.output_parent_path, args.dataset, args.cnn, image_name[:-5], "prompt.txt"), "w") as f:
             f.write(prompt)
-        if EXPLANATION_TYPE == "rigid":
+        if args.explanation_type == "rigid":
             explanation_file_name = "rigid_explanation.txt"
-        elif EXPLANATION_TYPE == "soft":
+        elif args.explanation_type == "soft":
             explanation_file_name = "soft_explanation.txt"
-        with open(os.path.join(OUTPUT_PARENT_PATH, DATASET, CNN, image_name[:-5], explanation_file_name), "w") as f:
+        with open(os.path.join(args.output_parent_path, args.dataset, args.cnn, image_name[:-5], explanation_file_name), "w") as f:
             f.write(explanation)
-        if COPY_IMAGE and not os.path.isfile(os.path.join(OUTPUT_PARENT_PATH, DATASET, CNN, image_name[:-5], image_name)):
-            copy2(full_image_path, os.path.join(OUTPUT_PARENT_PATH, DATASET, CNN, image_name[:-5], image_name))
+        if not args.do_not_copy_image and not os.path.isfile(os.path.join(args.output_parent_path, args.dataset, args.cnn, image_name[:-5], image_name)):
+            copy2(full_image_path, os.path.join(args.output_parent_path, args.dataset, args.cnn, image_name[:-5], image_name))
 
 
 if __name__ == "__main__":
-    Main()
+    parser = ArgumentParser(description='Run pipeline on whole validation set')
+    parser.add_argument("--val_set_parent_path", default="./val")
+    parser.add_argument("--val_images_names_full_path", default="./metadata/val_paths_experiments_0.csv")
+    parser.add_argument("--milan_descriptions_full_path", default="./milan_results/resnet18_imagenet.csv")
+    parser.add_argument("--openai_api_token_full_path", default="/home/adamwsl/.gpt_api_token/token.txt")
+    parser.add_argument("--cnn", default="resnet18")
+    parser.add_argument("--dataset", default="imagenet")
+    parser.add_argument("--output_parent_path", default="./basic_explanations")
+    parser.add_argument("--explanation_type", default="soft")
+    parser.add_argument("--do_not_copy_image", action="store_true", default=False)
+    parser.add_argument("--dataset_class_names", default="./metadata/imagenet_classes.txt")
+    args = parser.parse_args()
     
+    Main(args)
