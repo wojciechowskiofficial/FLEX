4d90c3ee5608df344b787de1daa3d42030367fe4
Adam Wojciechowski <adi61044@gmail.com>
2023-08-19 19:30:06 +0200

    code written, not checked

--- score_4_1.py
+++ score_4_1.py
@@ -1,49 +1,91 @@
-from src.male import run_pipeline_single_decision
 import os
 import pandas as pd
 from tqdm import tqdm
-from numpy import save
-from shutil import copy2
 from argparse import ArgumentParser
 from sacrebleu import sentence_bleu
 from evaluate import load
+import numpy as np
 
 
 def Main(args):    
-    a = "Your string a herefsdaaaaaaaaaaaaaaaeeiajoiejfoaijeofijasiofjeioafjoeisahfiuejashhifgueahioufgheioashfgiouehjsaoifhjeoiashfoeishaho"
-    b = 'The model identified this image as a "mosquito net" because it detected items with straight features at the top-right corner and right of the image. Additionally, it found a gusher of water and a ship, reflective surfaces, and red and blue objects throughout the entire image. The model also detected animals and screens at the top, top-right corner, center, and right of the image. Furthermore, it identified beds, items with circular features, and rounded edges in pictures throughout the entire image. Although the model did not find any human hands, it did detect animals and clothes in the upper half of the image.'
-
-    # BLEU Score
-    bleu = sentence_bleu(b, [a]).score
-    print(f"BLEU Score: {bleu}")
-
-    # BertSCORE
     bertscore = load("bertscore")
-    results = bertscore.compute(predictions=[a], references=[b], lang="en")
-    print(results)
-
-    # METEOR Score
-    #meteor = load('meteor')
-    #print(results)
-
+    meteorscore = load('meteor')
 
     df = pd.read_csv(args.val_images_names_full_path, header=None)
     image_names = df.iloc[:,0].tolist()
     
-    for noise_intensity in [.05]:
+    # Inter
+    for noise_intensity in [.05, .2]:
         variation = f"noise_int_{noise_intensity}"
+        bleu_list, bert_p_list, bert_r_list, bert_f1_list, meteor_list = [], [], [], [], []
         # Explain
         for image_name in tqdm(image_names):
-            full_image_path = os.path.join(args.val_set_parent_path, image_name)
-            
             with open(os.path.join(args.output_parent_path, args.dataset, args.cnn, image_name[:-5], variation, "soft_explanation.txt"), "r") as f:
                 noise_exp = f.read().strip()
             
-            with open(os.path.join("basic_explanations", args.dataset, args.cnn, image_name[:-5], "soft_explanation.txt"), "r") as f:
+            with open(os.path.join("basic_explanations_gpt4", args.dataset, args.cnn, image_name[:-5], "soft_explanation.txt"), "r") as f:
                 og_exp = f.read().strip()
             
-            print(noise_exp, og_exp)
-            break
+            bleu_list.append(sentence_bleu(hypothesis=noise_exp, references=[og_exp]).score)
+            bert_dict = bertscore.compute(predictions=[noise_exp], references=[og_exp], lang="en")
+            bert_p_list.append(bert_dict["precision"])
+            bert_r_list.append(bert_dict["recall"])
+            bert_f1_list.append(bert_dict["f1"])
+            meteor_list.append(meteorscore.compute(predictions=[noise_exp], references=[og_exp], lang="en"))
+            
+        with open(f"4_1_scores_inter_{variation}.csv", "w") as f:
+            f.write("metric,mean,std")
+            f.write("\n")
+            f.write(f"BLEU,{np.mean(bleu_list)},{np.std(bleu_list)}")
+            f.write("\n")
+            f.write(f"BERT_P,{np.mean(bert_p_list)},{np.std(bert_p_list)}")
+            f.write("\n")
+            f.write(f"BERT_R,{np.mean(bert_r_list)},{np.std(bert_r_list)}")
+            f.write("\n")
+            f.write(f"BERT_F1,{np.mean(bert_f1_list)},{np.std(bert_f1_list)}")
+            f.write("\n")
+            f.write(f"METEOR,{np.mean(meteor_list)},{np.std(meteor_list)}")
+
+    
+    # Intra
+    for noise_intensity in [.05, .2]:
+        variation = f"noise_int_{noise_intensity}"
+        bleu_list, bert_p_list, bert_r_list, bert_f1_list, meteor_list = [], [], [], [], []
+        # Explain
+        for first, second in zip(image_names[::2], image_names[1::2]):
+            with open(os.path.join(args.output_parent_path, args.dataset, args.cnn, first[:-5], variation, "soft_explanation.txt"), "r") as f:
+                noise_exp_first = f.read().strip()
+                
+            with open(os.path.join(args.output_parent_path, args.dataset, args.cnn, second[:-5], variation, "soft_explanation.txt"), "r") as f:
+                noise_exp_second = f.read().strip()
+            
+            bleu_list.append(sentence_bleu(hypothesis=noise_exp_first, references=[noise_exp_second]).score)
+            bert_dict = bertscore.compute(predictions=[noise_exp_first], references=[noise_exp_second], lang="en")
+            bert_p_list.append(bert_dict["precision"])
+            bert_r_list.append(bert_dict["recall"])
+            bert_f1_list.append(bert_dict["f1"])
+            meteor_list.append(meteorscore.compute(predictions=[noise_exp_first], references=[noise_exp_second], lang="en"))
+            
+            # reverse
+            bleu_list.append(sentence_bleu(hypothesis=noise_exp_second, references=[noise_exp_first]).score)
+            bert_dict = bertscore.compute(predictions=[noise_exp_second], references=[noise_exp_first], lang="en")
+            bert_p_list.append(bert_dict["precision"])
+            bert_r_list.append(bert_dict["recall"])
+            bert_f1_list.append(bert_dict["f1"])
+            meteor_list.append(meteorscore.compute(predictions=[noise_exp_second], references=[noise_exp_first], lang="en"))
+            
+        with open(f"4_1_scores_intra_{variation}.csv", "w") as f:
+            f.write("metric,mean,std")
+            f.write("\n")
+            f.write(f"BLEU,{np.mean(bleu_list)},{np.std(bleu_list)}")
+            f.write("\n")
+            f.write(f"BERT_P,{np.mean(bert_p_list)},{np.std(bert_p_list)}")
+            f.write("\n")
+            f.write(f"BERT_R,{np.mean(bert_r_list)},{np.std(bert_r_list)}")
+            f.write("\n")
+            f.write(f"BERT_F1,{np.mean(bert_f1_list)},{np.std(bert_f1_list)}")
+            f.write("\n")
+            f.write(f"METEOR,{np.mean(meteor_list)},{np.std(meteor_list)}")
 
 
 
